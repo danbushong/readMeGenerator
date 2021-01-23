270efe2b0d45a0308f23f355000c3e15c4eba09a
Dan Bushong <bushongdan@gmail.com>
2021-01-22 19:32:27 -0600

    setting up prompts const and axios

--- index.js
+++ index.js
@@ -1,25 +1,81 @@
-const inquirer = require('inquirer');
+generateMarkdown = require("./assets/js/generateMarkdown.js");
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    },
-    {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
-    },
-    {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
-    },
+
+
+const inquirer = require("inquirer");
+const fs =require("fs");
+
+const axios = require("axios")
+
+function userInputs(){
+    inquirer
+    //keep the prompt
+  .prompt([{
+    type: "input",
+    message: "What is your Github username?",
+    name: "userName"
+},
+{
+    type: "input",
+    message: "What is your email address?",
+    name: "userEmail",
+},
+{
+    type: "input",
+    message: "What is your project title?",
+    name: "projectTitle",
+},
+{
+    type: "input",
+    message: "What is the description of this project?",
+    name: "projectDescription",
+},
+{
+    type: "input",
+    message: "What is the path for this project's screenshot?",
+    default: "assets/img/screenshot.gif",
+    name: "projectImgSRC",
+},
+{
+    type: "input",
+    message: "What command should be run to install dependencies?",
+    default: "`npm install`",
+    name: "projectInstall",
+},
+{
+    type: "input",
+    message: "What command should be run to run the program?",
+    default: "`node index.js`",
+    name: "projectRun",
+},
+{
+    type: "input",
+    message: "What command should be run to start tests?",
+    default: "`npm test`",
+    name: "projectTest",
+},
+{
+    type: "input",
+    message: "Any additional information about the project?",
+    name: "projectInfo",
+},
+{
+    type: "input",
+    message: "Please input an appropriate license type for this project",
+    default: "MIT",
+    name: "projectLicense",
+}
+
+
+
+  
+
+
   ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+
+
+
+
+
+
+}
