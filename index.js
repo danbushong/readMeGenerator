ea59f3e5c5ccff26a2a511f9a6169a71d847dd07
Dan Bushong <bushongdan@gmail.com>
2021-01-24 14:46:58 -0600

    simplified a ton

--- index.js
+++ index.js
@@ -3,86 +3,92 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const axios = require("axios");
 
 
+const questions = [
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "userName"
+    },
+    {
+        type: 'input',
+        name: 'fullName',
+        message: 'Enter your  name.'
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "userEmail",
+    },
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "projectTitle",
+    },
+    {
+        type: "input",
+        message: "What is the description of this project?",
+        name: "projectDescription",
+    },
+    {
+        //this should be a different input
+        type: "input",
+        name: "projectLicense",
+        message: "Please input an appropriate license type for this project",
+        choices: [
+            "MIT",
+            "Apache2.0",
+            "GNUv3.0",
 
+        ]
+    },
+    {
+        type: 'input',
+        name: 'year',
+        message: 'What is the copywrite year?'
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        default: "`npm install`",
+        name: "projectInstall",
+    },
+    {
+        type: "input",
+        message: "What command should be run to start tests?",
+        default: "`npm test`",
+        name: "projectTest",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "projectRepoInfo",
+    },
+    {
+        type: "input",
+        message: "Any additional information about contributing to the repo?",
+        name: "projectRepoInfoTwo",
+    }
 
-function userInputs(){
-    inquirer
-    //keep the prompt
-  .prompt([{
-      
-    type: "input",
-    message: "What is your Github username?",
-    name: "userName"
-},
-{
-    type: 'input',
-    name: 'fullName',
-    message: 'Enter your  name.'
-},
-{
-    type: "input",
-    message: "What is your email address?",
-    name: "userEmail",
-},
-{
-    type: "input",
-    message: "What is your project title?",
-    name: "projectTitle",
-},
-{
-    type: "input",
-    message: "What is the description of this project?",
-    name: "projectDescription",
-},
-{
-    //this should be a different input
-    type: "input",
-    name: "projectLicense",
-    message: "Please input an appropriate license type for this project",
-    choices:[
-        "MIT",
-        "Apache2.0",
-        "GNUv3.0",    
-        
-    ]
-},
-{
-    type: 'input',
-    name: 'year',
-    message: 'What is the copywrite year?'
-},
-{
-    type: "input",
-    message: "What command should be run to install dependencies?",
-    default: "`npm install`",
-    name: "projectInstall",
-},
-{
-    type: "input",
-    message: "What command should be run to start tests?",
-    default: "`npm test`",
-    name: "projectTest",
-},
-{
-    type: "input",
-    message: "What does the user need to know about using the repo?",
-    name: "projectRepoInfo",
-},
-{
-    type: "input",
-    message: "Any additional information about contributing to the repo?",
-    name: "projectRepoInfoTwo",
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err =>
+        err ? console.log(err) : console.log('Success!'))
 }
 
-
-
-
-])
-
-
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile('README.md', data)
+        })
 }
+
+init();
+
+
+
+
 
 
